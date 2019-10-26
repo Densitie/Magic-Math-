9c092215dc069d06679e3be03ac1f8a5e489bef2
Densitie <55253212+Densitie@users.noreply.github.com>
2019-10-26 14:45:52 -0700

    Update MagicMath.java

--- src/MagicMath.java
+++ src/MagicMath.java
@@ -1,12 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a math program using Math class 
+ * October 26 2019
  */
 
 /**
  *
- * @author ajayk
+ * @author arkir7294
  */
 public class MagicMath extends javax.swing.JFrame {
     int given1;
